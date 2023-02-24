122935708b79e305570b3e53204c6b958d78332d
Julius_Kiama_Ndirangu <alvinjuliuskiamar@gmail.com>
2023-02-23 16:58:41 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Description: This function prints a message to the machine using the 
-  *              puts() function. 
-  * 
-  * Return: Always 0 
- */ 
-  
- int main(void) 
- { 
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints a message to the machine using the puts() function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
