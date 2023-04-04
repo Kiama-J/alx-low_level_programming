9166a67ac48f1835d2171144833f681c64b4e722
Julius_Kiama_Ndirangu <alvinjuliuskiamar@gmail.com>
2023-04-04 13:34:46 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - .
+ * @h: .
+ *
+ * Return: .
+ */
+
+size_t print_listint(const listint_t *h)
+
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
