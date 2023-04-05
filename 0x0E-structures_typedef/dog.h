e5ea522bf038aaddf8484c090196fc304a86c569
Julius_Kiama_Ndirangu <alvinjuliuskiamar@gmail.com>
2023-04-05 08:18:44 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - .
+ * @name: .
+ * @age: .
+ * @owner: .
+ * Description: .
+ */
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
